e576456ea6dc4749eca1e55021bbdb098c4c71bc
Wifu <62569962+Wifus@users.noreply.github.com>
2023-03-03 15:42:45 -0800

    Fixing copy paste oversight

--- src/utils/global.js
+++ src/utils/global.js
@@ -3,9 +3,9 @@ const { roles: ROLES } = require("../config.json");
 
 exports.hasRole = (member, role) => member?.roles.includes(role);
 exports.isHelper = (member) => exports.hasRole(member, ROLES.helper);
-exports.isModerator = (member) => exports.hasRole(member, ROLES.helper);
-exports.isAdmin = (member) => exports.hasRole(member, ROLES.helper);
-exports.isOwner = (member) => exports.hasRole(member, ROLES.helper);
+exports.isModerator = (member) => exports.hasRole(member, ROLES.mod);
+exports.isAdmin = (member) => exports.hasRole(member, ROLES.admin);
+exports.isOwner = (member) => exports.hasRole(member, ROLES.owner);
 exports.isStaff = (member) => exports.isHelper(member) || exports.isModerator(member) || exports.isAdmin(member) || exports.isOwner(member);
 
 exports.removeBadWords = function (text) {
