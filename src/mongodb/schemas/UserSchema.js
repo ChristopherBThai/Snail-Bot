8e7390b5989c325fe5d14deeced048d68ff1fcf5
Wifu <62569962+Wifus@users.noreply.github.com>
2023-03-03 20:29:44 -0800

    Update database schema

--- src/mongodb/schemas/UserSchema.js
+++ src/mongodb/schemas/UserSchema.js
@@ -1,22 +1,8 @@
 const mongoose = require('mongoose');
 
-const RoleSchema = require('./RoleSchema.js');
-
 const UserSchema = new mongoose.Schema({
 	_id: String,
-
-	roleBenefit: {
-		months: { type: Number, default: 0 },
-		started: { type: Date, default: Date.now },
-	},
-	role: { type: RoleSchema.schema, default: null },
-
-	friends: {
-		type: Map,
-		of: { type: String, ref: 'User' },
-	},
-
-	hoursBanned: { type: Number, default: 0 },
+	friends: [String],
 });
 
 module.exports = { name: 'User', schema: UserSchema };
