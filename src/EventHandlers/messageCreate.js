537862ba2170662cfc2b5c161544fc7490377758
Wifu <62569962+Wifus@users.noreply.github.com>
2023-03-03 16:42:47 -0800

    Cleaned up check mention code with new utils

--- src/EventHandlers/messageCreate.js
+++ src/EventHandlers/messageCreate.js
@@ -16,7 +16,7 @@ module.exports = class MessageCreateHandler {
 			msg.args = args.splice(1);
 			this.command.execute(msg);
 		}
-		this.checkModMention(msg);
+		this.checkStaffMention(msg);
 	}
 
 	hasPrefix(content) {
@@ -28,54 +28,38 @@ module.exports = class MessageCreateHandler {
 		}
 	}
 
-	async checkModMention(msg) {
-		if (msg.mentions && msg.mentions.length > 0) {
-			if (
-				global.hasRoles(msg.member, this.bot.config.roles.mods) ||
-				global.hasRoles(msg.member, this.bot.config.roles.helpers) ||
-				this.bot.config.channels.ignoreMention.includes(msg.channel.id)
-			) {
-				return;
-			}
-			msg.mentions.forEach(async (mention) => {
-				const member = msg.channel.guild.members.get(mention.id);
-				if (
-					global.hasRoles(member, this.bot.config.roles.mods) ||
-					global.hasRoles(member, this.bot.config.roles.helpers)
-				) {
-					// Tags dnd or offline helper/mod
-					if (
-						member.status === 'dnd' ||
-						member.status === 'offline' ||
-						!member.status
-					) {
-						const user = await this.bot.db.User.findById(mention.id);
-						if (user.friends && user.friends.has(msg.author.id)) return;
-						let warnMsg = await msg.channel.createMessage(
-							`⚠️ **|** ${msg.author.mention}, please refrain from tagging \`offline\` or \`do not disturb\` helpers/mods!`
-						);
-						const link = `https://discordapp.com/channels/${msg.channel.guild.id}/${msg.channel.id}/${warnMsg.id}`;
-						await this.bot.createMessage(
-							this.bot.config.channels.log,
-							`⚠️ **|** ${msg.author.mention} tagged ${member.username}#${member.discriminator} in ${msg.channel.mention} ${link}`
-						);
-					}
+	async checkStaffMention(msg) {
+		if (msg.mentions?.length == 0) return;											// Ignore if there are no mentions,
+		if (global.isStaff(msg.member)) return;											// was sent by a staff member,  
+		if (this.bot.config.channels.ignoreMention.includes(msg.channel.id)) return;	// or was sent in a mention ignored channel (only quest help at the moment)
 
-					// Tags in spam channel
-					else if (this.bot.config.channels.spam.includes(msg.channel.id)) {
-						const user = await this.bot.db.User.findById(mention.id);
-						if (user.friends && user.friends.has(msg.author.id)) return;
-						let warnMsg = await msg.channel.createMessage(
-							`⚠️ **|** ${msg.author.mention}, please refrain from tagging helper/mods in spam channels!`
-						);
-						const link = `https://discordapp.com/channels/${msg.channel.guild.id}/${msg.channel.id}/${warnMsg.id}`;
-						await this.bot.createMessage(
-							this.bot.config.channels.log,
-							`⚠️ **|** ${msg.author.mention} tagged ${member.username}#${member.discriminator} in ${msg.channel.mention} ${link}`
-						);
-					}
-				}
-			});
+		let warning = `⚠️ **|** ${msg.author.mention}, please refrain from tagging \`offline\` or \`do not disturb\` helpers/mods!`;
+		let mentioned_staff = [];
+
+		for (const mention of msg.mentions) {
+			const member = msg.channel.guild.members.get(mention.id);
+
+			if (!global.isStaff(member)) continue;									// Ignore if the mentioned user wasn't staff
+
+			const user = await this.bot.db.User.findById(mention.id);
+			if (user?.friends?.has(msg.author.id)) continue;						// Ignore if the staff member has the user on their friend list 
+
+			let is_online = member.status == "online";								// If the staff member was online
+			let in_spam = this.bot.config.channels.spam.includes(msg.channel.id)	// If mentioned in a spam channel
+
+			if (!is_online || in_spam) mentioned_staff.push(member);
+			
+			if (in_spam) warning = `⚠️ **|** ${msg.author.mention}, please refrain from tagging helper/mods in spam channels!`;
+		}
+
+		if (mentioned_staff.length != 0) {
+			let warn_message = await msg.channel.createMessage(warning);
+
+			let log_message = mentioned_staff.map(
+				member => `⚠️ **|** ${msg.author.mention} tagged ${member.username}#${member.discriminator} in ${msg.channel.mention} ${warn_message.jumpLink}`
+			).join(`\n`);
+
+			await this.bot.log(log_message);
 		}
 	}
 };
