2e44675118eeff0d162fd503c5bf4ae0a693b692
Wifu <62569962+Wifus@users.noreply.github.com>
2023-03-03 16:55:13 -0800

    Remove debug code and unused deprecated util

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,13 +1,8 @@
-const filter = new (require('bad-words'))();
 const { roles: ROLES } = require("../config.json");
 
 exports.hasRole = (member, role) => member?.roles.includes(role);
 exports.isHelper = (member) => exports.hasRole(member, ROLES.helper);
 exports.isModerator = (member) => exports.hasRole(member, ROLES.mod);
-exports.isAdmin = (member) => exports.hasRole(member, ROLES.admin) || exports.hasRole(member, "781285019338407956");
+exports.isAdmin = (member) => exports.hasRole(member, ROLES.admin);
 exports.isOwner = (member) => exports.hasRole(member, ROLES.owner);
 exports.isStaff = (member) => exports.isHelper(member) || exports.isModerator(member) || exports.isAdmin(member) || exports.isOwner(member);
-
-exports.removeBadWords = function (text) {
-	return filter.clean(text);
-};
