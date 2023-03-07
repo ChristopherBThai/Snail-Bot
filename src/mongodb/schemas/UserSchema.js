411b74de2c0bd1aa19a70cf41f95ace4a5e27f5a
Wifu <62569962+Wifus@users.noreply.github.com>
2023-03-07 11:39:25 -0800

    Review change #1

--- src/mongodb/schemas/UserSchema.js
+++ src/mongodb/schemas/UserSchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
 	_id: String,
-	friends: [String],
+	friends: [{ type: String, ref: 'User' }],
 });
 
 module.exports = { name: 'User', schema: UserSchema };
