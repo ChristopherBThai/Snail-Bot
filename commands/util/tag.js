7aa9ea03fc59b50067dc79191e897631fe128116
Wifu <62569962+Wifus@users.noreply.github.com>
2026-05-17 13:52:40 -0700

    Abstracted Tag command flow

--- commands/util/tag.js
+++ commands/util/tag.js
@@ -16,82 +16,90 @@ module.exports = new Command({
         'tag delete gems',
     ],
     execute: async function (ctx) {
-        if (ctx.name == 'tags') {
-            const tags = (await ctx.mongo.Tag.find({}, { _id: 1 }).sort({ _id: 1 }).lean()).map(tag => `\`${tag._id}\``);
-
-            if (!tags.length) return await ctx.error('Oh no! I don\'t have any tags :(');
-
-            return await ctx.send({
-                embeds: [{
-                    title: `Tags (${tags.length})`,
-                    description: tags.join(' '),
-                    timestamp: new Date(),
-                    color: ctx.bot.config.colors.embed
-                }]
-            });
-        }
+        if (ctx.name == 'tags') return await displayTags(ctx);
 
         const subcommand = ctx.args.shift()?.toLowerCase();
-
         if (!subcommand) return await ctx.error('please provide a tag name!');
 
-        // Using tag, not updating
         if (!UPDATE_SUBCOMMANDS.includes(subcommand)) {
-            const tag = await ctx.mongo.Tag.findById(subcommand);
-            if (!tag) return await ctx.error('that tag does not exist!');
-
-            let message;
-            try {
-                message = JSON.parse(tag.data);
-            } catch {
-                message = tag.data;
-            }
-
-            return await ctx.send(message);
+            return await displayTag(ctx, subcommand);
         }
 
-        // Only managers+ can update tags
-        if (!hasManagerPerms(ctx.message.member)) return;
-
-        const name = ctx.args.shift()?.toLowerCase();
-        if (!name) return await ctx.error('please provide a tag name!');
-
-        if (!/^[a-z0-9]+$/.test(name)) {
-            return await ctx.error('tag names can only contain alphanumeric characters!');
-        }
-
-        const data = ctx.args.join(' ');
-        // TODO: Support attachments
-
-        switch (subcommand) {
-            case 'add': {
-                if (!data) return await ctx.error('please provide some data for the tag!');
-
-                const tag = await ctx.mongo.Tag.findById(name);
-                if (tag) return await ctx.error('that tag already exists!');
-
-                await ctx.mongo.Tag.create({ _id: name, data });
-                await ctx.send(`I created the tag \`${name}\`!`);
-                break;
-            }
-            case 'edit': {
-                if (!data) return await ctx.error('please provide some data for the tag!');
-
-                const tag = await ctx.mongo.Tag.findById(name);
-                if (!tag) return await ctx.error('that tag does not exist!');
-
-                await ctx.mongo.Tag.updateOne({ _id: name }, { data });
-                await ctx.send(`I updated the tag \`${name}\`!`);
-                break;
-            }
-            case 'delete': {
-                const tag = await ctx.mongo.Tag.findById(name);
-                if (!tag) return await ctx.error('that tag does not exist!');
-
-                await ctx.mongo.Tag.deleteOne({ _id: name });
-                await ctx.send(`I deleted the tag \`${name}\`!`);
-                break;
-            }
-        }
+        await manageTag(ctx, subcommand);
     },
 });
+
+async function displayTags(ctx) {
+    const tags = (await ctx.mongo.Tag.find({}, { _id: 1 }).sort({ _id: 1 }).lean()).map(tag => `\`${tag._id}\``);
+
+    if (!tags.length) return await ctx.error('Oh no! I don\'t have any tags :(');
+
+    await ctx.send({
+        embeds: [{
+            title: `Tags (${tags.length})`,
+            description: tags.join(' '),
+            timestamp: new Date(),
+            color: ctx.bot.config.colors.embed
+        }]
+    });
+}
+
+async function displayTag(ctx, name) {
+    const tag = await ctx.mongo.Tag.findById(name);
+    if (!tag) return await ctx.error('that tag does not exist!');
+
+    let message;
+    try {
+        message = JSON.parse(tag.data);
+    } catch {
+        message = tag.data;
+    }
+
+    await ctx.send(message);
+}
+
+async function manageTag(ctx, subcommand) {
+    // Only managers+ can update tags
+    if (!hasManagerPerms(ctx.message.member)) return;
+
+    const name = ctx.args.shift()?.toLowerCase();
+    if (!name) return await ctx.error('please provide a tag name!');
+
+    if (!/^[a-z0-9]+$/.test(name)) {
+        return await ctx.error('tag names can only contain alphanumeric characters!');
+    }
+
+    const data = ctx.args.join(' ');
+    // TODO: Support attachments
+
+    switch (subcommand) {
+        case 'add': {
+            if (!data) return await ctx.error('please provide some data for the tag!');
+
+            const tag = await ctx.mongo.Tag.findById(name);
+            if (tag) return await ctx.error('that tag already exists!');
+
+            await ctx.mongo.Tag.create({ _id: name, data });
+            await ctx.send(`I created the tag \`${name}\`!`);
+            break;
+        }
+        case 'edit': {
+            if (!data) return await ctx.error('please provide some data for the tag!');
+
+            const tag = await ctx.mongo.Tag.findById(name);
+            if (!tag) return await ctx.error('that tag does not exist!');
+
+            await ctx.mongo.Tag.updateOne({ _id: name }, { data });
+            await ctx.send(`I updated the tag \`${name}\`!`);
+            break;
+        }
+        case 'delete': {
+            const tag = await ctx.mongo.Tag.findById(name);
+            if (!tag) return await ctx.error('that tag does not exist!');
+
+            await ctx.mongo.Tag.deleteOne({ _id: name });
+            await ctx.send(`I deleted the tag \`${name}\`!`);
+            break;
+        }
+    }
+}
